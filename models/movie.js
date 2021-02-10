94f75a7608d16c9b4e5a53ff990e4e97337b7bd9
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-10 10:21:49 +0300

    added movie model

---
+++ models/movie.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Url должен быть валидным',
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Url должен быть валидным',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Url должен быть валидным',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    select: false,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
