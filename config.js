0d384585184734df75d549f92ab1e953b664ab96
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-10 10:09:04 +0300

    added config file

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const { NODE_ENV = 'development', PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/movies-explorer' } = process.env;
+
+module.exports = {
+  PORT,
+  MONGO_URL,
+  NODE_ENV,
+}
