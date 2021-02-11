4f1d2ceb429ebb759381319f0f768a03b8b4b8c9
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-11 10:37:14 +0300

    added auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const NotAuthorizedError = require('../errors/NotAthorizedError');
+const { JWT_SECRET } = require('../config');
+
+module.exports = (req, res, next) => {
+  const authorization = req.cookies.jwt;
+  let payload;
+  try {
+    payload = jwt.verify(authorization, JWT_SECRET);
+  } catch (e) {
+    const err = new NotAuthorizedError('Необходима авторизация');
+    next(err);
+  }
+  req.user = payload;
+  next();
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const { createUser, login, logout } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 router.post('/signup', createUser);
 router.post('/signin', login);
 
 // доступен после авторизации!
-router.use('/users', userRouter);
-router.get('/logout', logout);
+router.use('/users', auth, userRouter);
+router.get('/logout', auth, logout);
