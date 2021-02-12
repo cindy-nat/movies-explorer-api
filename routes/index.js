263c57a2f10b02671c9926f9307ba8792f086f16
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-12 11:01:37 +0300

    added movies router in index routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userRouter = require('./users');
+const movieRouter = require('./movies');
 const { createUser, login, logout } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
@@ -9,6 +10,7 @@ router.post('/signin', login);
 
 // доступен после авторизации!
 router.use('/users', auth, userRouter);
+router.use('/movies', auth, movieRouter);
 router.post('/logout', auth, logout);
 
 router.use('/*', (req, res, next) => {
