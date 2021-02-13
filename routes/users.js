6c6d81f38104c56b24ad64cc63491c82c8a2fa61
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-13 20:49:51 +0300

    changed to patch user change

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const routerUsers = require('express').Router();
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
 
 routerUsers.get('/me', getUserInfo);
-routerUsers.put('/me', updateUserInfo);
+routerUsers.patch('/me', updateUserInfo);
 
 module.exports = routerUsers;
