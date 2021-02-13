47d2e26b2d3c0b45011b361a2900d23a36304550
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-13 20:58:42 +0300

    added controller for delete movieById

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,6 +2,7 @@ const movie = require('../models/movie');
 const NotFoundError = require('../errors/NotFoundError');
 const NotCorrectDataError = require('../errors/NotCorrectDataError');
 const ExistsError = require('../errors/ExistsError');
+const NoRightsError = require('../errors/NoRightsError');
 const { OK_CODE } = require('../errors/ErrorsCodes');
 
 const getMovies = (req, res, next) => {
@@ -14,8 +15,8 @@ const getMovies = (req, res, next) => {
 }
 
 const createMovieHandler = (req, res, next) => {
-  req.body.owner = req.user._id;
-  movie.create(req.body)
+  const { country, director, duration, year, description, image, trailer, movieId, nameRU, nameEN, thumbnail } = req.body;
+  movie.create({ country, director, duration, year, description, image, trailer, movieId, nameRU, nameEN, thumbnail, owner: req.user._id })
     .then((movieItem) => {
       if (!movieItem) { throw new NotCorrectDataError('Переданы некорректные данные для добавления фильма'); }
       res.status(OK_CODE).send(movieItem);
@@ -47,8 +48,19 @@ const createMovie = (req, res, next) => {
 }
 
 const removeMovie = (req, res, next) => {
-
-}
+  const { movieId } = req.params;
+  movie.findById(movieId)
+    .orFail(new NotFoundError('Карточка не найдена'))
+    .then((movieItem) => {
+      if (movieItem.owner.toString() !== req.user._id) {
+        throw new NoRightsError('Вы пытаетесь удалить чужую карточку');
+      } else {
+        movie.deleteOne(movieItem)
+          .then((deletedCard) => res.status(OK_CODE).send(deletedCard));
+      }
+    })
+    .catch(next);
+};
 
 module.exports = {
   getMovies,
