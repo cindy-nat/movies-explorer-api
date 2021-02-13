281e4e94c5310dc92c5e799f9470dbc5df0b4038
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-13 22:36:17 +0300

    added request and errors logging

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const errorsHandler = require('./middlewares/errorsHandler');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGO_URL } = require('./config');
 
 const app = express();
@@ -17,8 +18,12 @@ mongoose.connect(MONGO_URL, {
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(requestLogger);
+
 app.use(routes);
 
+app.use(errorLogger);
+app.use(errors());
 app.use(errorsHandler);
 
 app.listen(PORT, () => {
